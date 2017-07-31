5ae333b866c542fcf8a9b4a128d2cee523b9eb1f
Belinda <b.bowlyjung@gmail.com>
2017-07-31 15:44:24 +0700

    remove comment

--- server.js
+++ server.js
@@ -12,22 +12,21 @@ app.post('/api/translate', (req, res) => {
     let lang = req.body.lang? req.body.lang : 'en'
     translate(req.body.message, {to: lang}).then(response => {
         //console.log(response.text);
-        //=> I speak English 
         //console.log(response.from.language.iso);
         let send = {
             isSuccess: true,
             message: response.text
         }
         res.send(send)
-        //=> nl 
+        
     }).catch(err => {
-        // console.error(err);
         let send = {
             isSuccess: false,
             error: err,
             message: ''
         }
         res.send(send)
+        
     });
 })
 
